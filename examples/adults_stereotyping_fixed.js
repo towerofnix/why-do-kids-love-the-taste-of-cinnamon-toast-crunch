61e6d85201eeee731f3b0b550b62242ffbddc790
eq <eq@users.noreply.github.com>
2017-10-23 11:32:22 -0500

    Include adults

---
+++ examples/adults_stereotyping_fixed.js
@@ -0,0 +1,8 @@
+const getReasonWhyKidsLoveTheTasteOfCinnamonToastCrunch = require(
+  'why-do-kids-love-the-taste-of-cinnamon-toast-crunch')
+
+// Don't stereotype!!!111!!!!!!!!eleven1!!
+const getReasonWhyMostKidsAndAdultsLoveTheTasteOfCinnamonToastCrunch =
+      getReasonWhyKidsLoveTheTasteOfCinnamonToastCrunch.andAdults.iMeanMost
+
+console.log(getReasonWhyMostKidsAndAdultsLoveTheTasteOfCinnamonToastCrunch())
---
+++ examples/hello_adults.js
@@ -0,0 +1,4 @@
+const getReasonWhyKidsLoveTheTasteOfCinnamonToastCrunch = require(
+  'why-do-kids-love-the-taste-of-cinnamon-toast-crunch')
+
+console.log(getReasonWhyKidsLoveTheTasteOfCinnamonToastCrunch.andAdults())
